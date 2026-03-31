ff955bd5b029e300a731e9b6a5c90af174e104e6
Kaviyaa Vasudevan <120343718+kaviyaav@users.noreply.github.com>
2026-03-31 17:22:12 -0400

    Update images.py

--- App/images.py
+++ App/images.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 
-imagekit = ImageKit(
+imagekit_client = ImageKit(
     private_key=os.getenv("IMAGEKIT_PRIVATE_KEY"),
     public_key=os.getenv("IMAGEKIT_PUBLIC_KEY"),
     url_endpoint=os.getenv("IMAGEKIT_URL"),
